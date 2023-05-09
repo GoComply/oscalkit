db34819d8a4c1801ef0654d69c533ed5bc92018f
Šimon Lukašík <isimluk@fedoraproject.org>
2023-05-09 21:56:18 +0200

    lint plug:  Error return value of `cmd.Execute` is not checked (errcheck)

--- cli/gocomply_oscalkit/main.go
+++ cli/gocomply_oscalkit/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gocomply/oscalkit/cli/cmd"
 )
 
 func main() {
-	cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Troubles. %v", err)
+	}
 }
